5348483cb7e4ed99da44241bc1d698c5ada6be15
gsaidov <gulomjonsaidov@gmail.com>
2020-12-12 09:57:04 -0800

    testing heroku

---
+++ src/components/FooterComponent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+        return(
+            <footer className="site-footer">
+                <div>
+                    <h3>This will my Footer!!!!</h3>
+                    <p>The footer will be awesome</p>
+                </div>
+            </footer>
+            
+        );
+    }
+}
+
+export default Footer;
