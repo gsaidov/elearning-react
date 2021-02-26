a975af226ba51e78d7a2dcb241297bb6b1a9deb9
gsaidov <gulomjonsaidov@gmail.com>
2021-02-26 11:16:30 -0800

    CoureInfoComponent details

--- src/components/CourseInfoComponent.js
+++ src/components/CourseInfoComponent.js
@@ -20,9 +20,73 @@ class CourseInfo extends React.Component {
     );
   }
 
+  renderSyllabus(course) {
+    const parts = course.syllabus.map((part) => {
+      return (
+        <div key={part.id} className="row my-5 py-2">
+          <div className="col-md-3 h2 ">Part {part.id} </div>
+          <div className="col-md-9 text-justify py-3 border-bottom">
+            <h3 className="font-weight-bold">{part.name}</h3>
+            {part.description}
+          </div>
+        </div>
+      );
+    });
+    return parts;
+  }
+
+  courseDetails(course) {
+    return (
+      <>
+        <div className="container details-header">
+          <div className="row pt-3 ">
+            <div className="col-md-5 h2 course-name">
+              <strong>{course.name}</strong>
+            </div>
+            <div className="col-md-2 ml-auto">
+              Offered By:
+              <br />
+              {course.instructor}
+            </div>
+          </div>
+          <div className="row">
+            <div className="col h4 course-level">{course.level}</div>
+          </div>
+          <div className="row mt-5">
+            <div className="col-md-6">{course.description}</div>
+          </div>
+          <div className="row mt-5">
+            <div className="col">
+              <button className="btn btn-success my-4">
+                <strong>Enroll</strong>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="container mt-3">
+          <h3>
+            <strong>About this Course</strong>
+          </h3>
+          <p className="py-3 text-justify">{course.about}</p>
+          <h3>
+            <strong>Syllabus: What You will Learn</strong>
+          </h3>
+          <div className="container">
+            {this.renderSyllabus(this.props.course)}
+          </div>
+        </div>
+      </>
+    );
+  }
+
   render() {
     if (this.props.course) {
-      return <div className="row">{this.renderCourse(this.props.course)}</div>;
+      return (
+        <>
+          {/* <div className="row">{this.renderCourse(this.props.course)}</div> */}
+          {this.courseDetails(this.props.course)}
+        </>
+      );
     } else {
       return <div />;
     }
--- src/shared/courses.js
+++ src/shared/courses.js
@@ -4,35 +4,147 @@ export const COURSES = [
     name: "Single Variable Calculus",
     image: "/assets/images/calculus.jpg",
     instructor: "Gulom Saidov",
-    level: "beginner",
+    level: "Beginner",
     description:
       "This course covers the main ideas of single-variable Calculus with conceptual understanding and its applications.",
+    about:
+      "The focus and themes of the Introduction to Calculus course address the most important foundations for applications of mathematics in science, engineering and commerce. The course emphasises the key ideas and historical motivation for calculus, while at the same time striking a balance between theory and application, leading to a mastery of key threshold concepts in foundational mathematics.",
+    syllabus: [
+      {
+        id: 0,
+        name: "Precalculus",
+        description:
+          "This module begins by looking at the different kinds of numbers that fall on the real number line, decimal expansions and approximations, then continues with an exploration of manipulation of equations and inequalities, of sign diagrams and the use of the Cartesian plane.",
+      },
+      {
+        id: 1,
+        name: "Functions",
+        description:
+          "This module introduces the notion of a function which captures precisely ways in which different quantities or measurements are linked together. The module covers quadratic, cubic and general power and polynomial functions; exponential and logarithmic functions; and trigonometric functions related to the mathematics of periodic behaviour. We create new functions using composition and inversion and look at how to move backwards and forwards between quantities algebraically, as well as visually, with transformations in the xy-plane.",
+      },
+      {
+        id: 2,
+        name: "Introducing the differential calculus",
+        description:
+          "This module introduces techniques of differential calculus. We look at average rates of change which become instantaneous, as time intervals become vanishingly small, leading to the notion of a derivative. We then explore techniques involving differentials that exploit tangent lines. The module introduces Leibniz notation and shows how to use it to get information easily about the derivative of a function and how to apply it.",
+      },
+      {
+        id: 3,
+        name: "Properties and applications of the derivative",
+        description:
+          "This module continues the development of differential calculus by introducing the first and second derivatives of a function. We use sign diagrams of the first and second derivatives and from this, develop a systematic protocol for curve sketching. The module also introduces rules for finding derivatives of complicated functions built from simpler functions, using the Chain Rule, the Product Rule, and the Quotient Rule, and how to exploit information about the derivative to solve difficult optimisation problems.",
+      },
+    ],
   },
   {
     id: 1,
     name: "Advanced Javascript",
     image: "/assets/images/javascript.png",
     instructor: "Andrei Ng",
-    level: "advanced",
+    level: "Advanced",
     description:
       "The course covers the core concepts in theory of Javascript and extended with practical coding exercises.",
+    about:
+      "The focus and themes of the Introduction to Calculus course address the most important foundations for applications of mathematics in science, engineering and commerce. The course emphasises the key ideas and historical motivation for calculus, while at the same time striking a balance between theory and application, leading to a mastery of key threshold concepts in foundational mathematics.",
+    syllabus: [
+      {
+        id: 0,
+        name: "Precalculus",
+        description:
+          "This module begins by looking at the different kinds of numbers that fall on the real number line, decimal expansions and approximations, then continues with an exploration of manipulation of equations and inequalities, of sign diagrams and the use of the Cartesian plane.",
+      },
+      {
+        id: 1,
+        name: "Functions",
+        description:
+          "This module introduces the notion of a function which captures precisely ways in which different quantities or measurements are linked together. The module covers quadratic, cubic and general power and polynomial functions; exponential and logarithmic functions; and trigonometric functions related to the mathematics of periodic behaviour. We create new functions using composition and inversion and look at how to move backwards and forwards between quantities algebraically, as well as visually, with transformations in the xy-plane.",
+      },
+      {
+        id: 2,
+        name: "Introducing the differential calculus",
+        description:
+          "This module introduces techniques of differential calculus. We look at average rates of change which become instantaneous, as time intervals become vanishingly small, leading to the notion of a derivative. We then explore techniques involving differentials that exploit tangent lines. The module introduces Leibniz notation and shows how to use it to get information easily about the derivative of a function and how to apply it.",
+      },
+      {
+        id: 3,
+        name: "Properties and applications of the derivative",
+        description:
+          "This module continues the development of differential calculus by introducing the first and second derivatives of a function. We use sign diagrams of the first and second derivatives and from this, develop a systematic protocol for curve sketching. The module also introduces rules for finding derivatives of complicated functions built from simpler functions, using the Chain Rule, the Product Rule, and the Quotient Rule, and how to exploit information about the derivative to solve difficult optimisation problems.",
+      },
+    ],
   },
   {
     id: 2,
     name: "Python",
     image: "/assets/images/python.jpg",
     instructor: "Mike Brown",
-    level: "beginner",
+    level: "Beginner",
     description:
       "This Python course teaches the basics of how to construct a program from instructions in Python and build a simple application.",
+    about:
+      "This course aims to teach everyone the basics of programming computers using Python. We cover the basics of how one constructs a program from a series of simple instructions in Python.  The course has no pre-requisites and avoids all but the simplest mathematics. Anyone with moderate computer experience should be able to master the materials in this course. This course will cover Chapters 1-5 of the textbook “Python for Everybody”.  Once a student completes this course, they will be ready to take more advanced programming courses. This course covers Python 3.",
+    syllabus: [
+      {
+        id: 0,
+        name: "Precalculus",
+        description:
+          "This module begins by looking at the different kinds of numbers that fall on the real number line, decimal expansions and approximations, then continues with an exploration of manipulation of equations and inequalities, of sign diagrams and the use of the Cartesian plane.",
+      },
+      {
+        id: 1,
+        name: "Functions",
+        description:
+          "This module introduces the notion of a function which captures precisely ways in which different quantities or measurements are linked together. The module covers quadratic, cubic and general power and polynomial functions; exponential and logarithmic functions; and trigonometric functions related to the mathematics of periodic behaviour. We create new functions using composition and inversion and look at how to move backwards and forwards between quantities algebraically, as well as visually, with transformations in the xy-plane.",
+      },
+      {
+        id: 2,
+        name: "Introducing the differential calculus",
+        description:
+          "This module introduces techniques of differential calculus. We look at average rates of change which become instantaneous, as time intervals become vanishingly small, leading to the notion of a derivative. We then explore techniques involving differentials that exploit tangent lines. The module introduces Leibniz notation and shows how to use it to get information easily about the derivative of a function and how to apply it.",
+      },
+      {
+        id: 3,
+        name: "Properties and applications of the derivative",
+        description:
+          "This module continues the development of differential calculus by introducing the first and second derivatives of a function. We use sign diagrams of the first and second derivatives and from this, develop a systematic protocol for curve sketching. The module also introduces rules for finding derivatives of complicated functions built from simpler functions, using the Chain Rule, the Product Rule, and the Quotient Rule, and how to exploit information about the derivative to solve difficult optimisation problems.",
+      },
+    ],
   },
   {
     id: 3,
     name: "Machine Learning",
     image: "/assets/images/machine-learning.jpg",
     instructor: "Gerard Kim",
-    level: "intermediate",
+    level: "Intermediate",
     description:
       "This course provides a broad introduction to machine learning with topics in Supervised and Unsupervised Learning.",
+    about:
+      "The focus and themes of the Introduction to Calculus course address the most important foundations for applications of mathematics in science, engineering and commerce. The course emphasises the key ideas and historical motivation for calculus, while at the same time striking a balance between theory and application, leading to a mastery of key threshold concepts in foundational mathematics.",
+    syllabus: [
+      {
+        id: 0,
+        name: "Precalculus",
+        description:
+          "This module begins by looking at the different kinds of numbers that fall on the real number line, decimal expansions and approximations, then continues with an exploration of manipulation of equations and inequalities, of sign diagrams and the use of the Cartesian plane.",
+      },
+      {
+        id: 1,
+        name: "Functions",
+        description:
+          "This module introduces the notion of a function which captures precisely ways in which different quantities or measurements are linked together. The module covers quadratic, cubic and general power and polynomial functions; exponential and logarithmic functions; and trigonometric functions related to the mathematics of periodic behaviour. We create new functions using composition and inversion and look at how to move backwards and forwards between quantities algebraically, as well as visually, with transformations in the xy-plane.",
+      },
+      {
+        id: 2,
+        name: "Introducing the differential calculus",
+        description:
+          "This module introduces techniques of differential calculus. We look at average rates of change which become instantaneous, as time intervals become vanishingly small, leading to the notion of a derivative. We then explore techniques involving differentials that exploit tangent lines. The module introduces Leibniz notation and shows how to use it to get information easily about the derivative of a function and how to apply it.",
+      },
+      {
+        id: 3,
+        name: "Properties and applications of the derivative",
+        description:
+          "This module continues the development of differential calculus by introducing the first and second derivatives of a function. We use sign diagrams of the first and second derivatives and from this, develop a systematic protocol for curve sketching. The module also introduces rules for finding derivatives of complicated functions built from simpler functions, using the Chain Rule, the Product Rule, and the Quotient Rule, and how to exploit information about the derivative to solve difficult optimisation problems.",
+      },
+    ],
   },
 ];
